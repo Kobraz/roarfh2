2c64cbf768094d3c25bea8e0eaa276d4f262fc56
Kobraz <roar.f.hanssen@gmail.com>
2023-04-16 16:32:45 +0200

    Update api1.js

--- js/api1.js
+++ js/api1.js
@@ -41,7 +41,7 @@ function createProductHTML(product) {
 
 function createProductsHTML(products) {
     for (let i = 0; i < products.length; i++) {
-        const product = products[0];
+        const product = products[4];
         createProductHTML(product);
     }
 }
